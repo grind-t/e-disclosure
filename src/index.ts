5bb7b4072b07c9eb080f37b9f045c9f11e8f7276
grind-t <grindt.gd@gmail.com>
2026-09-01 01:24:17 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { chromium } from "patchright";
+
+const browser = await chromium.launchPersistentContext(".session", {
+  channel: "chrome",
+  headless: false,
+  viewport: null,
+});
+
+const page = await browser.newPage()
+
+await page.goto('https://www.e-disclosure.ru/')
